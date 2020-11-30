f7eb8305c7aab81564805825d7a1d702a5ea5f6d
Yujun Zhang <yujunz@nvidia.com>
2020-11-30 13:42:16 +0800

    Add alias ksops-exec

--- hack/custom/user-init.sh
+++ hack/custom/user-init.sh
@@ -9,8 +9,10 @@ helm2 init --client-only && \
 export KUSTOMIZE_PLUGIN_PATH=${XDG_CONFIG_HOME}/kustomize/plugin
 
 export KSOPS_VERSION=2.2.2
-export KSOPS_PATH=${KUSTOMIZE_PLUGIN_PATH}/viaduct.ai/v1
+export KSOPS_PATH=${KUSTOMIZE_PLUGIN_PATH}/viaduct.ai/v1/ksops
 mkdir -p $KSOPS_PATH
 curl -sSL --fail https://github.com/viaduct-ai/kustomize-sops/releases/download/v${KSOPS_VERSION}/ksops_${KSOPS_VERSION}_Linux_x86_64.tar.gz \
   | tar xzf - -C ${KSOPS_PATH} ksops
 chmod +x ${KSOPS_PATH}/ksops
+mkdir -p ${KSOPS_PATH}-exec
+ln -s ../ksops/ksops ${KSOPS_PATH}-exec/ksops-exec
